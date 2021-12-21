a7463ea587dc5c0ea07a8540a3377b14e50325ff
Leonardo.Selmini <selminileonardo@gmail.com>
2021-12-21 18:41:54 +0100

    milestone 3 - covers and star rating

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,16 +3,20 @@
     <ul>
       <li v-for="(film, index) in globalData.films" :key="index">
         <div class="info" v-if="globalData.select == 'movie'">
+          <img v-if="film.poster_path != null" :src="'https://image.tmdb.org/t/p/w154' + film.poster_path" :alt="film.title">
+          <img v-else :src="'https://image.tmdb.org/t/p/w154' + film.backdrop_path" :alt="film.title">
           <h2>{{film.title}}</h2>
           <p>{{film.release_date}}</p>
           <p v-html="'Lingua originale:' + flagIt(film.original_language)"></p>
-          <p>{{film.vote_average}}</p>
+          <p v-html="starRating(film.vote_average)"></p>
         </div>
         <div class="info" v-else>
+          <img v-if="film.poster_path != null" :src="'https://image.tmdb.org/t/p/w154' + film.poster_path" :alt="film.name">
+          <img v-else :src="'https://image.tmdb.org/t/p/w154' + film.backdrop_path" :alt="film.title">
           <h2>{{film.name}}</h2>
           <p>{{film.first_air_date}}</p>
           <p v-html="'Lingua originale:' + flagIt(film.original_language)"></p>
-          <p>{{film.vote_average}}</p>
+          <p v-html="starRating(film.vote_average)"></p>
         </div>
       </li>
     </ul>
@@ -35,13 +39,16 @@ export default {
   methods: {
     flagIt(countryCode) {
       if (countryCode == 'en'){
-        countryCode = 'gb'
+        countryCode = 'gb';
       } else if ( countryCode == 'ja'){
-        countryCode = 'jp'
+        countryCode = 'jp';
       }
       return countryCode.toUpperCase().replace(/./g, char => 
           String.fromCodePoint(127397 + char.charCodeAt())
       );
+    },
+    starRating(rating){
+      return '&#11088;'.repeat(Math.floor(rating / 2))
     }
   }
 }
